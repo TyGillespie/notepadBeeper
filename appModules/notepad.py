7b640053743aed94e1bb1163c67a72e3e77f0fe7
TyGillespie <60036684+TyGillespie@users.noreply.github.com>
2020-09-08 21:01:41 -0600

    2020.1 version
    Updated to 2020.1

--- appModules/notepad.py
+++ appModules/notepad.py
@@ -6,10 +6,24 @@
 #
 # Initial code was from the NVDA Developers guide, with some modifications. 
 # Plans to expand this project are certainly out there. 
+#
+# Change log
+#
+# 2020.1 (8 September, 2020)
+# Added a new command (NVDA plus Shift plus L to speak the current line. 
 import appModuleHandler
 import tones
+import api
+import ui
+from scriptHandler import script
 class AppModule(appModuleHandler.AppModule):
 	def event_gainFocus(self, obj, nextHandler):
 		# The focus has been changed, so beep the tone at 500 hz, and for 50 MS. 
 		tones.beep(500, 50)
 		nextHandler()
+	@script(gesture="kb:NVDA+shift+l")
+	def script_sayLineNumber(self, gesture):
+		# We're using two nested functioncalls. api.getStatusBarText(obj) and api.getStatusBar(). api.getStatusBar() returns an NVDAObjects.NVDAObject, and api.getStatusBarText(obj) returns the status bar text of the given obj, if it's a status bar. This is how we get the data to split for the line number. 
+		lineNumList = api.getStatusBarText(api.getStatusBar()).split()
+		lineNum = lineNumList[2] + lineNumList[3]
+		ui.message(lineNum)
