b29a1e96d0834484f4e3023f941d426aa2bc2f0e
TyGillespie <60036684+TyGillespie@users.noreply.github.com>
2021-01-28 16:09:26 -0700

    Moved changes into their own file.

---
+++ CHANGES.md
@@ -0,0 +1,10 @@
+# Changes.
+
+## 2020.1 (8 September, 2020)
+
+* Added a new command (NVDA plus Shift plus L to speak the current line.
+
+## 2020.2, 22 September, 2020
+
+*  Formatted code for pep-8
+* Added Control+NVDA+Shift+N to open an untitled Notepad file.
--- appModules/notepad.py
+++ appModules/notepad.py
@@ -5,14 +5,6 @@
 #
 # Initial code was from the NVDA Developers guide, with some modifications.
 # Plans to expand this project are certainly out there.
-#
-# Change log
-#
-# 2020.1 (8 September, 2020)
-# Added a new command (NVDA plus Shift plus L to speak the current line.
-# 2020.2, 22 September, 2020
-# Formatted code for pep-8
-# Added Control+NVDA+Shift+N to open an untitled Notepad file.
 
 """Main appModule.
 """
